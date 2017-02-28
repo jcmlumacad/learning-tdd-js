2c329417f90860d19643b8506844d58050b0f586
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-02-28 15:05:42 +0800

    refactor changes

--- app.js
+++ app.js
@@ -9,26 +9,72 @@
     function SampleController(SampleFactory) {
         var vm = this;
 
-        vm.login = {
-            email: 'john@doe.com',
-            password: '123456'
-        };
-        vm.register = {
-            email: 'jane@doe.com',
-            password: '123456'
-        };
+        vm.login = login;
+        vm.register = register;
         vm.users = SampleFactory.all();
+
+        function login(params) {
+            return SampleFactory.authenticate(params);
+        }
+
+        function register(params) {
+            return SampleFactory.save(params);
+        }
     }
 
     function SampleFactory() {
         var factory = {
-            all: allUsers
+            users: [],
+            errorMessages: [],
+            authenticate: authenticate,
+            all: all,
+            save: save
         };
 
         return factory;
 
-        function allUsers() {
-            return [];
+        function all() {
+            return factory.users;
+        }
+
+        function save(params) {
+            if ( ! ( !! params.name)) {
+                factory.errorMessages.push('The name field is required');
+            }
+            if ( ! ( !! params.email)) {
+                factory.errorMessages.push('The email field is required');
+            }
+            if ( (!! params.password_confirmation && ! ( !! params.password)) || ( ! (!! params.password_confirmation) && ! ( !! params.password))) {
+                factory.errorMessages.push('The password field is required');
+            }
+            if ( !! params.password && !! params.password_confirmation) {
+                if (params.password !== params.password_confirmation) {
+                    factory.errorMessages.push('The password confirmation does not match');
+                }
+            }
+            if ( !! params.password && ! ( !! params.password_confirmation)) {
+                factory.errorMessages.push('The password confirmation does not match');
+            }
+
+            if (factory.errorMessages.length === 0) {
+                factory.users.push(params);
+
+                return true;
+            }
+
+            return factory.errorMessages;
+        }
+
+        function authenticate(params) {
+            var isLoggedIn = false;
+
+            factory.users.forEach(function (user) {
+                if (user.email === params.email && user.password === params.password) {
+                    isLoggedIn = true;
+                }
+            });
+
+            return isLoggedIn;
         }
     }
 })();
--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -2,6 +2,12 @@
 
 describe('SampleController', function () {
     var controller, factory;
+    var data = {
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password: '123456',
+        password_confirmation: '123456'
+    };
 
     beforeEach(module('sample'));
 
@@ -11,134 +17,237 @@ describe('SampleController', function () {
 
     describe('login', function () {
         it('should login with correct credentials', function () {
-            expect(controller.users).toContain({
-                username: controller.login.username,
-                password: controller.login.password
-            });
+            var params = {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                password_confirmation: data.password_confirmation
+            };
+
+            expect(controller.register(params)).toBeTruthy();
+            expect(controller.login({
+                email: data.email,
+                password: data.password
+            })).toBeTruthy();
         });
 
         it('should not login with wrong credentials', function () {
-            expect(controller.users).not.toContain({
-                username: controller.login.username,
-                password: 'invalid-password' 
-            });
+            expect(controller.login({
+                email: data.email,
+                password: data.password
+            })).toBeFalsy();
         });
     });
 
     describe('register', function () {
-        describe('username', function () {
-            it('must be unique', function () {
-                var usernames = [];
-                controller.users.forEach(function (user) {
-                    usernames.push(user.username);
-                });
-                expect(usernames).not.toContain(controller.register.username);
-            });
-
-            it('contains letters', function () {
-                expect(controller.register.username).toMatch(/[a-zA-Z]/);
-            });
-
-            it('contains numbers', function () {
-                expect(controller.register.username).toMatch(/\d+/);
-            });
-
-            it('contains underscore', function () {
-                expect(controller.register.username).toMatch(/[_]/);
-            });
-
-            it('must have a minimum of 6 characters', function () {
-                expect(controller.register.username.length).toBeGreaterThan(5);
-            });
-        });
-
-        describe('password', function () {
-            it('contains letters', function () {
-                expect(controller.register.password).toMatch(/[a-z]/);
-            });
-
-            it('contains numbers', function () {
-                expect(controller.register.password).toMatch(/\d+/);
-            });
-
-            it('contains special characters', function () {
-                expect(controller.register.password).toMatch(/[$&+,:;=?@#/\\|'<>.^*()%!_"-]/);
-            });
-
-            it('must have atleast one uppercase letter', function () {
-                expect(controller.register.password).toMatch(/[A-Z]/);
-            });
-
-            it('must have a minimum of 8 characters', function () {
-                expect(controller.register.password.length).toBeGreaterThan(7);
-            });
-
-            it('must not the same as username', function () {
-                expect(controller.register.password).not.toEqual(controller.register.username);
-            });
-        });
-
         it('only name', function () {
+            var params = {
+                name: data.name
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The email field is required',
+                'The password field is required'
+            ]));
         });
 
         it('only email', function () {
+            var params = {
+                email: data.email
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The password field is required'
+            ]));
         });
 
         it('only password', function () {
+            var params = {
+                password: data.password
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The email field is required',
+                'The password confirmation does not match'
+            ]));
         });
 
         it('only confirm password', function () {
+            var params = {
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The email field is required',
+                'The password field is required'
+            ]));
         });
 
         it('only name and email', function () {
+            var params = {
+                name: data.name,
+                email: data.email
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The password field is required'
+            ]));
         });
 
         it('only name and password', function () {
+            var params = {
+                name: data.name,
+                password: data.password
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The email field is required',
+                'The password confirmation does not match'
+            ]));
         });
 
         it('only name and confirm password', function () {
+            var params = {
+                name: data.name,
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The email field is required',
+                'The password field is required'
+            ]));
         });
 
         it('only email and password', function () {
+            var params = {
+                email: data.email,
+                password: data.password
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The password confirmation does not match'
+            ]));
         });
 
         it('only email and confirm password', function () {
+            var params = {
+                email: data.email,
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The password field is required'
+            ]));
         });
 
         it('only password and confirmation password', function () {
+            var params = {
+                password: data.password,
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The email field is required'
+            ]));
         });
 
         it('all fields except name', function () {
+            var params = {
+                email: data.email,
+                password: data.password,
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required'
+            ]));
+        });
+
+        it('all fields except email', function () {
+            var params = {
+                name: data.email,
+                password: data.password,
+                password_confirmation: data.password_confirmation
+            };
+
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The email field is required'
+            ]));
         });
 
         it('all fields except password', function () {
+            var params = {
+                name: data.name,
+                email: data.email,
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The password field is required'
+            ]));
         });
 
         it('all fields except confirm password', function () {
+            var params = {
+                name: data.name,
+                email: data.email,
+                password: data.password
+            };
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The password confirmation does not match'
+            ]));
         });
 
         it('all fields', function () {
+            var params = {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                password_confirmation: data.password_confirmation
+            };
 
+            expect(controller.register(params)).toBeTruthy();
         });
 
         it('without inputs', function () {
+            var params = {};
 
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The name field is required',
+                'The email field is required',
+                'The password field is required'
+            ]));
         });
 
         it('password must not match to confirm password', function () {
+            var params = {
+                password: data.password,
+                password_confirmation: 'qwerty'
+            };
+
+            expect(controller.register(params)).toEqual(jasmine.arrayContaining([
+                'The password confirmation does not match'
+            ]));
+        });
+    });
+
+    describe('reset password', function () {
+        it('should email your password reset link', function () {
+
+        });
+
+        it('should not reset the password with the wrong token', function () {
+
+        });
+
+        it('should reset the password with the right token', function () {
 
         });
     });
